25030715290932f541f8fb70c263062d8968a08b
puruj <puru123456789@hotmail.com>
2025-03-03 22:24:20 -0500

    Recipe Card component is functional

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,5 +1,6 @@
-import { Heart, Search, Soup } from 'lucide-react'
+import { Heart, HeartPulse, Search, Soup } from 'lucide-react'
 import React from 'react'
+import RecipeCard from '../components/RecipeCard'
 
 const HomePage = () => {
   return (
@@ -19,18 +20,18 @@ const HomePage = () => {
         <p className='text-slate-500 font-semibold m1-1 my-2 text-sm tracking-tight'>Popular Choices</p>
         <div className='grid gap-3 grid-cols-1 md:grid-cols-2 lg:grid-cols-3'>
           {/* 1st recipe */}
-          <div className='flex flex-col rounded-md bg-[#ecf7d4] overflow-hidden p-3 relative'>
-            {/* recipe youtube video link */}
-            <a href='#' className='relative h-32'>
-            <img src='/1.jpg' alt='recipe img' className='rounded-md w-full h-full object-cover cursor-pointer'/>
-            <div className='absolute bottom-2 left-2 bg-white rounded-full p-1 cursor-pointer flex items-center gap-1 text-sm'>
-              <Soup size={16}/> 4 Servings
-            </div>
-            <div className='absolute top-2 right-2 bg-white rounded-full p-1 cursor-pointer'>
-              <Heart size={20} className='hover:fill-red-500 hover:text-red-500'/>
-            </div>
-            </a>
-          </div>
+         <RecipeCard />
+         <RecipeCard />
+         <RecipeCard />
+         <RecipeCard />
+         <RecipeCard />
+         <RecipeCard />
+         <RecipeCard />
+         <RecipeCard />
+         <RecipeCard />
+         <RecipeCard />
+         <RecipeCard />
+         <RecipeCard />
         </div>
       </div>
     </div>

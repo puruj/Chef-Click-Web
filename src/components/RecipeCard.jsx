9b03d8e73b76e9ceea41ddeccee3ccb5d7dc23de
puruj <puru123456789@hotmail.com>
2025-03-04 00:08:41 -0500

    Get and display some of the api recipes now

--- src/components/RecipeCard.jsx
+++ src/components/RecipeCard.jsx
@@ -1,23 +1,23 @@
 import { Heart, HeartPulse, Search, Soup } from 'lucide-react'
 import React from 'react'
 
-const RecipeCard = () => {
+const RecipeCard = ({recipe}) => {
   return (
     <div className='flex flex-col rounded-md bg-[#ecf7d4] overflow-hidden p-3 relative'>
     {/* recipe youtube video link */}
     <a href='#' className='relative h-32'>
-    <img src='/1.jpg' alt='recipe img' className='rounded-md w-full h-full object-cover cursor-pointer'/>
+    <img src={recipe.image} alt='recipe img' className='rounded-md w-full h-full object-cover cursor-pointer'/>
     <div className='absolute bottom-2 left-2 bg-white rounded-full p-1 cursor-pointer flex items-center gap-1 text-sm'>
-      <Soup size={16}/> 4 Servings
+      <Soup size={16}/> {recipe.yield} Servings
     </div>
     <div className='absolute top-2 right-2 bg-white rounded-full p-1 cursor-pointer'>
       <Heart size={20} className='hover:fill-red-500 hover:text-red-500'/>
     </div>
     </a>
     <div className='flex mt-1'>
-      <p className='font-bold traciking-wide'>Roasted Chicken</p>
+      <p className='font-bold traciking-wide'>{recipe.label}</p>
     </div>
-    <p className='my-2'>Turkish Kitchen</p>
+    <p className='my-2'>{recipe.cuisineType[0].charAt(0).toUpperCase() + recipe.cuisineType[0].slice(1)} Kitchen</p>
     <div className='flex gap-2 mt-auto'>
       <div className='flex gap-1 bg-[#d6f497] items-center p-1 rounded-md'>
         <HeartPulse size={16}/>
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,8 +1,42 @@
 import {Search } from 'lucide-react'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import RecipeCard from '../components/RecipeCard'
+import { getRandomColor } from '../lib/utils'
+
+const APP_ID="7c152676"
+const APP_KEY="b987c7bed1bcc6bd12e3ef5e2ee663fe"
 
 const HomePage = () => {
+  const [recipes, setRecipes] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  const fetchRecipes = async(searchQuery) => {
+    setLoading(true);
+    setRecipes([]);
+    try {
+      const res = await fetch(
+        `https://api.edamam.com/api/recipes/v2?type=public&q=${searchQuery}&app_id=${APP_ID}&app_key=${APP_KEY}`,
+        {
+          method: 'GET',
+          headers: {
+            'Accept': 'application/json',
+            'Edamam-Account-User': 'electron333', 
+            'Accept-Language': 'en'              
+          }
+        }
+      );
+      const data = await res.json();
+      setRecipes(data.hits);
+    } catch (error) {
+      console.log(error.message);
+    } finally{
+      setLoading(false);
+    }
+  }
+  useEffect(() => {
+    fetchRecipes("tofu");
+  }, []);
+
   return (
     <div className='bg-[#faf9fb] p-10 flex-1'>
       <div className='max-w-screen-lg mx-auto'>
@@ -19,8 +53,21 @@ const HomePage = () => {
         <h1 className='font-bold text-3xl md:text-5xl mt-4'>Recommended Recipes</h1>
         <p className='text-slate-500 font-semibold m1-1 my-2 text-sm tracking-tight'>Popular Choices</p>
         <div className='grid gap-3 grid-cols-1 md:grid-cols-2 lg:grid-cols-3'>
-          {/* 1st recipe */}
-         <RecipeCard />
+        {!loading &&
+						recipes.map(({ recipe }, index) => (
+							<RecipeCard key={index} recipe={recipe} {...getRandomColor} />
+						))}
+				{loading &&
+						[...Array(9)].map((_, index) => (
+							<div key={index} className='flex flex-col gap-4 w-full'>
+								<div className='skeleton h-32 w-full'></div>
+								<div className='flex justify-between'>
+									<div className='skeleton h-4 w-28'></div>
+									<div className='skeleton h-4 w-24'></div>
+								</div>
+								<div className='skeleton h-4 w-1/2'></div>
+							</div>
+						))}
         </div>
       </div>
     </div>
